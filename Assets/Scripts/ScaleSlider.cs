88af13a657c942ab8e8ee860fc5d21015a57fd70
Synergiance <syn@synerfiles.info>
2023-06-24 15:40:31 -0400

    Revision 1

---
+++ Assets/Scripts/ScaleSlider.cs
@@ -0,0 +1,23 @@
+﻿
+using UdonSharp;
+using UnityEngine.UI;
+
+public class ScaleSlider : UdonSharpBehaviour {
+	public Slider slider;
+	public Text value;
+	public Scaler scaler;
+	public ScaleAdjust scaleSelect;
+	public bool sendInitialValue;
+
+	public float Value => slider == null ? 0 : slider.value;
+
+	void Start() {
+		value.text = slider.value.ToString("N2");
+		if (sendInitialValue) scaler._AdjustScale(scaleSelect, slider.value);
+	}
+
+	public void _SetValue() {
+		value.text = slider.value.ToString("N2");
+		scaler._AdjustScale(scaleSelect, slider.value);
+	}
+}
---
+++ Assets/Scripts/Scaler.cs
@@ -0,0 +1,90 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+
+public enum ScaleAdjust {
+	MinimumHeight, MaximumHeight, EyeHeight, AvatarScale
+}
+
+public class Scaler : UdonSharpBehaviour {
+	public Toggle enableScalingToggle;
+	public Text initialHeightValue;
+	public Text currentHeightValue;
+	public Text currentScaleValue;
+
+	private float initialAvatarHeight;
+	private float currentAvatarHeight;
+	private float currentAvatarScale;
+
+	private float eyeHeightSetting;
+	private float avatarScaleSetting;
+
+	void Start() {
+		if (Networking.LocalPlayer == null) return;
+		SetInitialHeight(Networking.LocalPlayer.GetAvatarEyeHeightAsMeters());
+		_ToggleScaling();
+	}
+
+	public void OnAvatarChanged(VRCPlayerApi player) {
+		if (!Utilities.IsValid(player)) return;
+		if (!player.isLocal) return;
+		SetInitialHeight(player.GetAvatarEyeHeightAsMeters());
+	}
+
+	public void OnAvatarEyeHeightChanged(VRCPlayerApi player, float height) {
+		if (!Utilities.IsValid(player)) return;
+		if (!player.isLocal) return;
+		SetCurrentHeight(height);
+	}
+
+	public void _AdjustScale(ScaleAdjust scale, float value) {
+		if (Networking.LocalPlayer == null) return;
+		switch (scale) {
+			case ScaleAdjust.MinimumHeight:
+				Networking.LocalPlayer.SetAvatarEyeHeightMinimumByMeters(value);
+				break;
+			case ScaleAdjust.MaximumHeight:
+				Networking.LocalPlayer.SetAvatarEyeHeightMaximumByMeters(value);
+				break;
+			case ScaleAdjust.EyeHeight:
+				eyeHeightSetting = value;
+				break;
+			case ScaleAdjust.AvatarScale:
+				avatarScaleSetting = value;
+				break;
+			default:
+				Debug.LogError("Range Error: " + nameof(scale));
+				break;
+		}
+	}
+
+	public void _ApplyHeight() {
+		if (Networking.LocalPlayer == null) return;
+		Networking.LocalPlayer.SetAvatarEyeHeightByMeters(eyeHeightSetting);
+	}
+
+	public void _ApplyScale() {
+		if (Networking.LocalPlayer == null) return;
+		Networking.LocalPlayer.SetAvatarEyeHeightByMultiplier(avatarScaleSetting);
+	}
+
+	public void _ToggleScaling() {
+		Networking.LocalPlayer.SetManualAvatarScalingAllowed(enableScalingToggle.isOn);
+	}
+
+	private void SetInitialHeight(float height) {
+		initialAvatarHeight = height;
+		initialHeightValue.text = height.ToString("N2");
+		SetCurrentHeight(height);
+	}
+
+	private void SetCurrentHeight(float height) {
+		currentAvatarHeight = height;
+		currentHeightValue.text = height.ToString("N2");
+		currentAvatarScale = currentAvatarHeight / initialAvatarHeight;
+		currentScaleValue.text = currentAvatarScale.ToString("N2");
+	}
+}
